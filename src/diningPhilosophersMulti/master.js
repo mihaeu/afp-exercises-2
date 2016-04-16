3f8ce9f40e88e877e5009c69a8a66e0a2c296a7d
Marinus Noichl <marinus.noichl@stud.fh-rosenheim.de>
2016-04-16 11:48:44 +0200

    Setup interprocess communication for workers

--- src/diningPhilosophersMulti/master.js
+++ src/diningPhilosophersMulti/master.js
@@ -7,4 +7,17 @@ var philosopher3 = cp.fork(__dirname + '/worker3');
 var philosopher4 = cp.fork(__dirname + '/worker4');
 var philosopher5 = cp.fork(__dirname + '/worker5');
 
+let setupInterprocessCommunication = (publisher, subscribers) => {
+    publisher.on('message', function(data){
+        console.log(data);
+        for (let subscriber of subscribers){
+            subscriber.send(data);
+        }
+    });
+};
 
+setupInterprocessCommunication(philosopher1, [philosopher5, philosopher2]);
+setupInterprocessCommunication(philosopher2, [philosopher1, philosopher3]);
+setupInterprocessCommunication(philosopher3, [philosopher2, philosopher4]);
+setupInterprocessCommunication(philosopher4, [philosopher3, philosopher5]);
+setupInterprocessCommunication(philosopher5, [philosopher4, philosopher1]);
